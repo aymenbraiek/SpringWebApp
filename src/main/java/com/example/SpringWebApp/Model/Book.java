0f40e44fae5e319a3d15fdbc5c7afeb25c0bead9
aymenbraiek1 <aymen.braiek@esprit.tn>
2019-09-16 18:27:36 +0100

    ENTITY MANYTOMANY BIDIREC ONE TABLE

--- src/main/java/com/example/SpringWebApp/Model/Book.java
+++ src/main/java/com/example/SpringWebApp/Model/Book.java
@@ -1,12 +1,74 @@
 package com.example.SpringWebApp.Model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
-private String title;
-private String isbn;
-private String pubLisher;
-private Set<Author> authors=new HashSet<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String pubLisher;
+    @ManyToMany
+    @JoinTable (name="author_book",joinColumns= {@JoinColumn (name="book_id")},inverseJoinColumns = {@JoinColumn(name = "author_id")})
+    private Set<Author> authors = new HashSet<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String pubLisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.pubLisher = pubLisher;
+    }
+
+    public Book(String title, String isbn, String pubLisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.pubLisher = pubLisher;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPubLisher() {
+        return pubLisher;
+    }
+
+    public void setPubLisher(String pubLisher) {
+        this.pubLisher = pubLisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authorss) {
+        this.authors = authorss;
+    }
 }
