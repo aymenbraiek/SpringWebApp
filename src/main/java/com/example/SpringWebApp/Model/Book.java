dcd923ed234db34d44b257fa2ad31fa9c82265ca
aymenbraiek1 <aymen.braiek@esprit.tn>
2019-09-16 18:34:31 +0100

    ENTITY MANYTOMANY BIDIREC ONE TABLE

--- src/main/java/com/example/SpringWebApp/Model/Book.java
+++ src/main/java/com/example/SpringWebApp/Model/Book.java
@@ -2,6 +2,7 @@ package com.example.SpringWebApp.Model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -15,6 +16,30 @@ public class Book {
     @ManyToMany
     @JoinTable (name="author_book",joinColumns= {@JoinColumn (name="book_id")},inverseJoinColumns = {@JoinColumn(name = "author_id")})
     private Set<Author> authors = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", pubLisher='" + pubLisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 
     public Long getId() {
         return id;
